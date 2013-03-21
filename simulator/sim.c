2c8c92e00cb6dbdf7ff2da7095576a7b2462ba1d
apoorvingle <apoorv.ingle@gmail.com>
2013-03-21 13:10:49 +0530

    folder name change

---
+++ simulator/sim.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int memory[256];
+
+int main(){
+    int a, i;
+    a = 0b00001111;
+    printf("%d", a);
+    for (i = 0; i < 4; i += 1){
+        a<<
+    }
+    return 0;
+}
